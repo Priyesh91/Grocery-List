46092b647d70063dfc2d39081923d25b7ab2a6a3
Priyesh <91priyesh@gmail.com>
2019-04-17 11:12:11 -0400

    started work on navbar

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Grocery-List
 Grocery-List app parses data from JSON file to the web page keeping in mind responsiveness and web accessibility.
+
+
+
+
+
+
+##Credits
+Logo free image source
+https://www.kisspng.com/png-shopping-cart-online-shopping-retail-logo-cart-vec-1382400/download-png.html
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,58 @@
-console.log("hi")
+$(document).ready(function () {
+  //Using jQuery to access data from JSON file
+  $.getJSON("/assets/groceries.json", function (data) {
+    //gorceryList from JSON file
+    const groceryList = data;
+    // console.log(groceryList);
+
+    //individual items
+        groceryList.forEach(individualItem => {
+
+    // console.log(individualItem);
+    })
+
+    //creating array of categories
+    const categories = [];
+    groceryList.forEach( category => {
+      categories.push(category.category);
+    })
+    console.log(categories);
+
+    //creating arra
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
+});
